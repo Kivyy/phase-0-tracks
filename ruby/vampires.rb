2bb1daf95d00e502e207e6ee533507336a47acd5
Kiv Lo <kivlokw@gmail.com>
2017-03-08 16:10:55 -0500

    Add condition matching

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,5 +13,38 @@ garlic_test = gets.chomp == "y" ? true : false
 puts "Would you like to enroll in the company's health insurance? (y/n)"
 health_insurance = gets.chomp == "y" ? true : false 
 
+# using this method to calculate user actual age and test if user_name is upcase.
+
+def age_calculator(user_birth_year)
+	date = Time.now.year 
+	actual_age = date - user_birth_year 
+end 
+
+user_real_age = age_calculator(user_birth_year) 
+
+def check_name(user_name)
+	return true if user_name === "Drake Cula"
+	return true if user_name === "Tu Fang"
+	return false
+end 
+
+name_test = check_name(user_name)
+
+
+# Condition matching starts here
+
+if user_age == user_real_age && (garlic_test || health_insurance) && !name_test
+	puts "Probably not a vampire"
+elsif user_age != user_real_age && (garlic_test || health_insurance)
+	puts "Probably a vampire"
+elsif user_age != user_real_age && !garlic_test && !health_insurance
+	puts "Almost certainly a vampire"
+elsif user_age == user_real_age && garlic_test && health_insurance && name_test 
+	puts "Definitely a vampire."
+else 
+	puts "Results inconclusive"
+end 
+
+
 
 
