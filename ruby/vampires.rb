2252e75482555c7682effdae0fe2d018bf50ef1b
Kiv Lo <kivlokw@gmail.com>
2017-03-08 16:39:06 -0500

    Add loop to survey

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,48 +1,56 @@
-puts "What is your name?"
-user_name = gets.chomp
+puts "How many employees will be processed?"
+employee_count = gets.chomp.to_i
 
-puts "What year were you born?"
-user_birth_year = gets.chomp.to_i 
+until employee_count == 0 
 
-puts "and how old are you?"
-user_age = gets.chomp.to_i
+employee_count -= 1 
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?(y/n)"
-garlic_test = gets.chomp == "y" ? true : false 
+	puts "What is your name?"
+	user_name = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance? (y/n)"
-health_insurance = gets.chomp == "y" ? true : false 
+	puts "What year were you born?"
+	user_birth_year = gets.chomp.to_i 
 
-# using this method to calculate user actual age and test if user_name is upcase.
+	puts "and how old are you?"
+	user_age = gets.chomp.to_i
 
-def age_calculator(user_birth_year)
-	date = Time.now.year 
-	actual_age = date - user_birth_year 
-end 
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?(y/n)"
+	garlic_test = gets.chomp == "y" ? true : false 
 
-user_real_age = age_calculator(user_birth_year) 
+	puts "Would you like to enroll in the company's health insurance? (y/n)"
+	health_insurance = gets.chomp == "y" ? true : false 
 
-def check_name(user_name)
-	return true if user_name === "Drake Cula"
-	return true if user_name === "Tu Fang"
-	return false
-end 
+	# using this method to calculate user actual age and test if user_name is upcase.
 
-name_test = check_name(user_name)
+	def age_calculator(user_birth_year)
+		date = Time.now.year 
+		actual_age = date - user_birth_year 
+	end 
+
+	user_real_age = age_calculator(user_birth_year) 
+
+	def check_name(user_name)
+		return true if user_name === "Drake Cula"
+		return true if user_name === "Tu Fang"
+		return false
+	end 
+
+	name_test = check_name(user_name)
 
 
-# Condition matching starts here
+	# Condition matching starts here
 
-if user_age == user_real_age && (garlic_test || health_insurance) && !name_test
-	puts "Probably not a vampire"
-elsif user_age != user_real_age && (garlic_test || health_insurance)
-	puts "Probably a vampire"
-elsif user_age != user_real_age && !garlic_test && !health_insurance
-	puts "Almost certainly a vampire"
-elsif user_age == user_real_age && garlic_test && health_insurance && name_test 
-	puts "Definitely a vampire."
-else 
-	puts "Results inconclusive"
+	if user_age == user_real_age && (garlic_test || health_insurance) && !name_test
+		puts "#{user_name}is probably not a vampire"
+	elsif user_age != user_real_age && (garlic_test || health_insurance)
+		puts "#{user_name}is probably a vampire"
+	elsif user_age != user_real_age && !garlic_test && !health_insurance
+		puts  "#{user_name}is almost certainly a vampire"
+	elsif user_age == user_real_age && garlic_test && health_insurance && name_test 
+		puts "#{user_name}is definitely a vampire."
+	else 
+		puts "Results inconclusive"
+	end 
 end 
 
 
