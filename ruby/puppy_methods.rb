1f42525bbb2c5c29cc14fc299bab75f4b63266fb
Kiv Lo <kivlokw@gmail.com>
2017-03-21 12:16:06 -0400

    Add speak method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,8 +4,17 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
-
+  
+  def speak(barkcounts)
+    bark_length = barkcounts.to_i 
+    bark_length.times do |bark|
+      puts "Woof!"
+    end
+  end
+  
 end
 
 dog = Puppy.new 
 dog.fetch("bone")
+dog.speak(6)
+
