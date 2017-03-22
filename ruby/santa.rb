2c8714899dfaacb287d29ac8edc7edfd503b1f16
Kiv Lo <kivlokw@gmail.com>
2017-03-22 12:10:57 -0400

    Add santa class to doc.

---
+++ ruby/santa.rb
@@ -0,0 +1,18 @@
+
+#Release 0 
+
+class Santa 
+
+	def initialize
+		puts "Initalizing Santa instance..."
+	end 
+
+	def speak 
+		puts "Ho, ho, ho! Haaaapy Holidays!"
+	end 
+
+	def eat_milk_and_cookie(cookie_type)
+		puts "That was a good #{cookie_type}."
+	end
+
+end 
