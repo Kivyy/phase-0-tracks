6461774394a1fc7f79af22699439a9001d8b88b2
Kiv Lo <kivlokw@gmail.com>
2017-03-22 13:12:22 -0400

    Add loop to push elements into an empty santa array.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,19 @@
 
 #Release 0 
+#add a santa class with three method. 
+#eat_milk_and cookie method will pass on an argument and print out the string.
+
+#Release 1 
+#adding instance variables to initialize method. Gender and ethnicity will require arguments. Ranking and age will have default value.
 
 class Santa 
 
-	def initialize
+	def initialize(gender,ethnicity)
 		puts "Initalizing Santa instance..."
+		@gender = gender 
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0 
 	end 
 
 	def speak 
@@ -16,3 +25,17 @@ class Santa
 	end
 
 end 
+
+santas = []
+
+#First we have to create two array for gender and ethnicity.
+sant_genders = ["agender", "female", "bigender", "male", "female", "gender fluid","transgender"]
+sant_ethnicities = ["black", "Latino", "white", "Asian", "prefer not to say", "Mystical Creature (unicorn)", "I can be whatever"]
+# We will have to push these elements from the array to the empty santa array.
+# add random elements of the array to the new empty santa arry by using sampl method.
+
+sant_genders.length.times do |i|
+	santas << Santa.new(sant_genders.sample, sant_ethnicities.sample)
+end 
+
+p santas 
