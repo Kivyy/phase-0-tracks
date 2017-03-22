a6b9a19c8e212bba39a91e863ca7c109a83f5dd1
Kiv Lo <kivlokw@gmail.com>
2017-03-22 16:06:20 -0400

    Add loop to print out instances

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,14 +7,14 @@
 #adding instance variables to initialize method. Gender and ethnicity will require arguments. Ranking and age will have default value.
 
 class Santa 
-	attr_accessor gender: age: ethnicity: 
+	attr_accessor :gender, :age, :ethnicity 
 
 	def initialize(gender,ethnicity)
 		puts "Initalizing Santa instance..."
 		@gender = gender 
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0 
+		@age = rand(0..140)
 	end 
 
 	def speak 
@@ -39,18 +39,37 @@ class Santa
 
 end 
 
-santas = []
 
-#First we have to create two array for gender and ethnicity.
+#The codes below are for release 1 
+# #First we have to create two array for gender and ethnicity.
 sant_genders = ["agender", "female", "bigender", "male", "female", "gender fluid","transgender"]
 sant_ethnicities = ["black", "Latino", "white", "Asian", "prefer not to say", "Mystical Creature (unicorn)", "I can be whatever"]
-# We will have to push these elements from the array to the empty santa array.
-# add random elements of the array to the new empty santa arry by using sampl method.
+# # We will have to push these elements from the array to the empty santa array.
+# # add random elements of the array to the new empty santa arry by using sampl method.
 
-sant_genders.length.times do |i|
+# sant_genders.length.times do |i|
+# 	santas << Santa.new(sant_genders.sample, sant_ethnicities.sample)
+# end 
+
+
+santas = []
+
+#Release 4 
+#Create a program that create numbers of santa according to user input.
+puts "Hey, how many santa do you want to create for your SantaCon simulator"
+num_santa = gets.chomp.to_i 
+
+#The user input number will be how many santa instances we will be creating for the user.
+num_santa.times do |num|
 	santas << Santa.new(sant_genders.sample, sant_ethnicities.sample)
 end 
 
-p santas 
-
+#Each instances created for the array we can use an each loop to go through each instance and call out the instance varibales.
+santas.each do |n|
+	puts "----------------------"
+	puts "This santa gender: #{n.gender}. "
+	puts "This santa ethnicity: #{n.ethnicity}."
+	puts "This santa is #{n.celebrate_birthday} years old."
+	puts "----------------------"
+end 
 
