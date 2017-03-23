714df76f033288380d100eff7bea73d7f665ff5d
Kiv Lo <kivlokw@gmail.com>
2017-03-23 17:48:52 -0400

    Add 4 method for rpec test

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,13 @@
 module Shout 
-	#We'll put some methods here soon, but this code is fine for now!
+	def self.yell_angrily(words)
+		words + '!!!' + ' :C'
+	end 
+
+	def self.yell_happily(str)
+		str + '!' + ' :)'
+	end 
 end 
 
+
+Shout.yell_happily("hello world")
+Shout.yell_angrily("hello world")
