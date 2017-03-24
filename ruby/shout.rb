ef46fa2d1dbd19d6a4c85f4a20a4300c07626e70
Kiv Lo <kivlokw@gmail.com>
2017-03-24 10:50:32 -0400

    Add mixin module and two class with driver codes.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,33 @@
-module Shout 
-	def self.yell_angrily(words)
-		words + '!!!' + ' :C'
-	end 
+# module Shout 
+# 	def self.yell_angrily(words)
+# 		words + '!!!' + ' :C'
+# 	end 
 
-	def self.yell_happily(str)
-		str + '!' + ' :)'
+# 	def self.yell_happily(str)
+# 		str + '!' + ' :)'
+# 	end 
+# end 
+
+
+# Shout.yell_happily("hello world")
+# Shout.yell_angrily("hello world")
+
+module Shout 
+	def yell_angrily(string)
+		string + "!!!" + "OMG."
 	end 
 end 
 
+class Person 
+	include Shout 
+end 
 
-Shout.yell_happily("hello world")
-Shout.yell_angrily("hello world")
+class Parents 
+	include Shout
+end 
+
+person = Person.new 
+person.yell_angrily("Go away")
+
+mother = Parents.new 
+mother.yell_angrily("Clean your room")
