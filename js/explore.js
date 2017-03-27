addb2695e5b9c417d52b7648950ab9186d3afee0
Kiv Lo <kivlokw@gmail.com>
2017-03-27 16:15:10 -0400

    initialize doc with first-draft pseudocodes.

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// Relase 4: Write a Javascript Program 
+// a method that takes a string as argument 
+// turn string into an array 
+// switch index position from -1 to 0 , -2 to 1 ,etc and store it into a new var by using a loop to iterate the array. 
+// return string 
+
