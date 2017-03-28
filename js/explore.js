a9314d0bad9507492d83e2f123e21fb346d4417e
Kiv Lo <kivlokw@gmail.com>
2017-03-28 12:49:34 -0400

    fix syntax for method's first draft.

--- js/explore.js
+++ js/explore.js
@@ -1,9 +1,13 @@
 // Relase 4: Write a Javascript Program 
 // a method that takes a string as argument 
-function reverseStr(word);{
-	
-}
+function reverseStr(word) {
 // create a new variable to store the new string 
+var newStr = "";
+// iterate the string and store the last letter of the string to the new variable. 
+for (var i = 0; i < word.length; i ++){
+	newStr = ("" + word.charAt(word.length - 1 );
+}
+}
 // iterate the string and store the last letter of the string to the new variable. 
 // return string.
 
