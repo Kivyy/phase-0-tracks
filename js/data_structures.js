42be57ce435c67255d2374bd2766990f8ca4497c
Kiv Lo <kivlokw@gmail.com>
2017-03-29 15:07:44 -0400

    initialize doc and add two arrays.

---
+++ js/data_structures.js
@@ -0,0 +1,11 @@
+var color = ["red","blue","purple","green"];
+var names = ["speed","barry","allen","zoom"];
+
+console.log(color);
+color.push("yellow");
+console.log(color);
+
+console.log(names);
+names.push("peter");
+console.log(names);
+
