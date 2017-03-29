678a8ca76381e36b4675f4e70ed9a7f1092cdf4b
Kiv Lo <kivlokw@gmail.com>
2017-03-29 15:21:17 -0400

    add method to add key and values

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,11 +1,21 @@
 var color = ["red","blue","purple","green"];
 var names = ["speed","barry","allen","zoom"];
 
-console.log(color);
-color.push("yellow");
-console.log(color);
+// console.log(color);
+// color.push("yellow");
+// console.log(color);
 
-console.log(names);
-names.push("peter");
-console.log(names);
+// console.log(names);
+// names.push("peter");
+// console.log(names);
+
+var horse = {};
+
+if (color.length == names.length){
+	for(var i = 0; i < color.length; i++){
+		horse[names[i]]= color[i];
+	}
+}
+
+console.log(horse);
 
