ffa44f0aabe3d9fe87551605aea696c800f13e9d
Kiv Lo <kivlokw@gmail.com>
2017-04-02 20:23:50 -0400

    add another method to check the property of the array.

--- js/algos.js
+++ js/algos.js
@@ -19,3 +19,23 @@ function checkLongWord(wordArray){
 // Driver codes 
 var cars = ["honda","toyota","mitsubushi","ford","mercedez"];
 console.log(checkLongWord(cars));
+
+//Release 1 
+// a method to check if a pair of objects have at least one key-value pair. (ex. {name:bob , age: 34} {name: Dillan age:34} should return true.)
+// if no key-value matches then return false. 
+// found a javascript loop method that will go through property of the array(no hash in javascript, iits call object literal.)
+	// this function will take two array to check their properties( = key and values in Ruby.)
+
+var personOne = {name:"Dillan",age:25};
+var personTwo = {name:"Francis",age:25};
+
+function checkProp(arrayOne,arrayTwo){
+  for (property in arrayOne){
+    if(arrayOne[property] == arrayTwo[property]){
+      return true;
+    }
+  }
+  return false;
+}
+
+checkProp(personOne,personTwo)
