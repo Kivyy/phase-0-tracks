37bc8e7ab54c58e1870e0431bd936f68b6128383
Kiv Lo <kivlokw@gmail.com>
2017-04-07 12:48:39 -0400

    Add table to database.

--- databases/Sqlruby/password.rb
+++ databases/Sqlruby/password.rb
@@ -3,3 +3,13 @@ require 'faker'
 require 'sqlite3'
 
 datab = SQLite3::Database.new("password_reminder.db")
+
+create_password_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS passwords(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		password VARCHAR(255)
+	)
+SQL
+
+datab.execute(create_password_cmd)
