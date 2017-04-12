dc99067ac8b82eeb39e1331752982038334a2acc
Kiv Lo <kivlokw@gmail.com>
2017-04-12 15:55:21 -0400

    Add functions and basic html element.

--- web_dev/client_side_js/JS_dom.js
+++ web_dev/client_side_js/JS_dom.js
@@ -1,7 +1,23 @@
 // Create element
 
-function createElement(element){
-	var ele = document.createElement(element);
-	ele.style.height = '100vh';
-	document.body.appendChild(ele);
+function createPara(element){
+	var newElement = document.createElement(element);
+	newElement.style.height = "100vh";
+	document.body.appendChild(newElement);
+	newElement.textContent = 'Working with DOM is fun!'
+	return newElement;
 }
+
+var paraOne = createPara('P');
+
+function sendAlert() {
+    setInterval(function(){ alert("IT WORKSSS!"); }, 2000);
+}
+
+
+function addBorder(event){
+	paraOne.style.border = "2px solid black";
+}
+paraOne.addEventListener("click",addBorder);
+
+
